ef28e027be31e93bafb8654da4a8e86249772103
Soton <77579218+soton371@users.noreply.github.com>
2021-04-18 13:28:25 +0600

    Add files via upload

---
+++ webvi.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+
+class WebV extends StatefulWidget {
+  @override
+  _WebVState createState() => _WebVState();
+}
+
+class _WebVState extends State<WebV> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebView(
+        initialUrl: 'https://digitalnews71.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+        gestureNavigationEnabled: true,
+
+      ),
+    );
+  }
+}
+
